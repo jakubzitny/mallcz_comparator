eb5e70960efefec056b1392c20e6c3381733a424
Dávid Ocetník <ocetnik.david@gmail.com>
2017-12-01 22:34:07 +0100

    Extract chart into separate component

--- src/Product.js
+++ src/Product.js
@@ -1,35 +1,6 @@
 import React, { Component } from 'react';
 import { Checkbox, Header, Menu } from 'semantic-ui-react';
-import { Scatter } from 'react-chartjs-2';
-
-const data = {
-  labels: ['Scatter'],
-  datasets: [
-    {
-      label: 'My First dataSet',
-      fill: false,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      // pointBorderWidth: 1,
-      // pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      // pointHoverBorderWidth: 2,
-      // pointRadius: 1,
-      // pointHitRadius: 10,
-      data: [
-        { x: 65, y: 75 },
-        { x: 59, y: 49 },
-        { x: 80, y: 90 },
-        { x: 81, y: 29 },
-        { x: 56, y: 36 },
-        { x: 55, y: 25 },
-        { x: 40, y: 18 },
-      ]
-    }
-  ]
-};
+import Chart from './Chart';
 
 class Product extends Component {
   state = {
@@ -87,13 +58,7 @@ class Product extends Component {
         </Menu>
 
         <div className="product-content-container">
-          <div className="chart-canvas">
-            <Scatter
-              data={data}
-              getElementAtEvent={this.getElementAtEvent}
-              legend={{ display: false }}
-            />
-          </div>
+          <Chart />
         </div>
       </div>
     );
