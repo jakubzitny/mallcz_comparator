02504f26087739cd657ef8466844ea1a12e24472
Jakub Žitný <jakubzitny@avocode.com>
2017-12-02 18:54:09 +0100

    bump

--- src/utils/data-utils.js
+++ src/utils/data-utils.js
@@ -1,20 +1,49 @@
 export default class DataUtils {
   static filterDataByPrice(data, sliderValues) {
+    // if (!data || !data.results) {
+    //   return null
+    // }
+
     const results = data.results;
+    const baseProduct = results.find((result) => {
+      return result['IsBase']
+    })
+
     const nextResults = results.filter((result) => {
       const product = result['Product']
       if (result['IsBase']) {
-        console.log('YAYYYY')
         return true
       }
 
       const price = Number(product['PRICE_VAT'])
-      const valid = (
+      const score = Number(result['Score'])
+
+      const inRange = (
         Number(sliderValues[0]) <= price &&
         price <= Number(sliderValues[1])
       )
 
-      return valid
+      if (inRange) {
+        return true
+      }
+
+      // should be always true
+      // if (baseProduct) {
+      //   const baseProductPrice = Number(baseProduct['Product']['PRICE_VAT'])
+      //   const baseProductScore = Number(baseProduct['Score'])
+      //   const ratio = (
+      //     (price / baseProductPrice)
+      //     /
+      //     (score / baseProductScore)
+      //   )
+
+      //   if (ratio > 1) {
+      //     console.log(`${price}/${baseProductPrice} / ${score}/${baseProductScore} ~== ${ratio}`)
+      //     return true
+      //   }
+      // }
+
+      return false
     })
 
     data.results = nextResults;
